75d92263eb10a1f36f78743a89e6e96153407de6
Deswandy <deswandywong23@gmail.com>
2025-07-14 14:32:30 +0700

    added fraunces font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>landing</div>
+    <div>
+      <h1 className="font-fraunces font-bold text-4xl">Fraunces Bold</h1>
+      <p className="font-fraunces font-light italic">
+        Fraunces Light Italic with Variable Font
+      </p>
+      <p
+        className="font-fraunces"
+        style={{ fontVariationSettings: "'SOFT' 100, 'WONK' 1" }}
+      >
+        Fraunces with SOFT 100 & WONK 1
+      </p>
+
+    </div>
   )
 }
 
