14782024ba971eb72c280ce44de401ccae2f67d2
Deswandy <deswandywong23@gmail.com>
2025-07-16 17:35:13 +0700

    fixed shadcn config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 import Generouted from '@generouted/react-router/plugin'
@@ -6,4 +7,9 @@ import Generouted from '@generouted/react-router/plugin'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), Generouted()],
+    resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
