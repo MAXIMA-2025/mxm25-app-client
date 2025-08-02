09348ecfc766b4e3d27f41aa88cb06e1dc8b2b5e
Deswandy <deswandywong23@gmail.com>
2025-08-02 20:20:14 +0700

    feat: devtools tanstack

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Routes } from "@generouted/react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Toaster } from "sonner";
 
@@ -16,6 +17,7 @@ createRoot(document.getElementById("root")!).render(
       <AuthProvider>
         <Routes />
         <Toaster position="top-right" richColors closeButton/>
+        <ReactQueryDevtools initialIsOpen={false} />
       </AuthProvider>
     </QueryClientProvider>
   </StrictMode>
