aaba724822ff059e8b20aa91966c06fccbfd110c
Deswandy <deswandywong23@gmail.com>
2025-08-09 08:19:49 +0700

    feat: error page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,41 +1,41 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import Logo from "@/assets/images/logo.png";
-import HeroBackground from "@/assets/images/hero/BACKGROUND.webp";
-import HeroForegroundLeft from "@/assets/images/hero/FOREGROUND1.webp";
-import HeroForegroundRight from "@/assets/images/hero/FOREGROUND2.webp";
-import { Link } from "react-router";
+import Logo from "@/assets/images/logoDivisi/nexus.png";
 import { useNavigate } from "react-router";
+import { ArrowRight } from "lucide-react";
+import backgroundImage from "@/assets/images/onboarding.webp"
 
-const HeroSection = () => {
+const ErrorPage = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/login");
   };
   return (
-    <section className="w-dvw h-dvh flex justify-center items-center overflow-hidden relative">
-      <div className="flex flex-col-reverse justify-center items-center">
-        <div className="flex flex-col gap-2 z-1 p-5 items-center text-center">
-          <h1 className="font-fraunces font-semibold text-3xl md:text-4xl">
-            Explore with Curiosity,
-            <br /> Discover the Mistery!
+    <section
+      className="h-screen bg-black/60 w-full flex flex-col items-center gap-4 justify-center px-2 py-32 sm:px-4 md:px-8 bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        backgroundBlendMode: "darken",
+      }}
+    >
+      <div className="flex flex-col justify-center items-center">
+        <img className="w-34 z-1 md:w-40 drop-shadow-2xl" src={Logo} />
+        <div className="flex flex-col mt-2 gap-2 drop-shadow-2xl">
+          <h1 className="font-fraunces text-white text-center text-5xl font-semibold">
+            404 NOT FOUND
           </h1>
-          <div className="font-futura font-medium text-lg ">
-            <p className="text-center">Kenali kegiatan dan organisasi di UMN</p>
-            <p className="text-center">Yuk, kita menjelajah bersama-sama!</p>
-          </div>
-          <Button
-            className="w-[64%] md:w-[50%] mt-2"
-            variant="clay"
-            onClick={handleClick}
-          >
-            Jelajahi Bersama!
-          </Button>
+          <h1 className="font-futura text-white text-center text-xl font-medium">
+            Hai, kami Nexus, dan kamu nyasar.
+          </h1>
         </div>
-        <img className="w-34 z-1 md:w-40" src={Logo} />
+          <Button className="mt-2" variant="secondary" onClick={handleClick}>
+            Kembali ke website <ArrowRight />
+          </Button>
       </div>
     </section>
   );
 };
 
-export default HeroSection;
+export default ErrorPage;
