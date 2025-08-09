8030d1526e5a3624868e6b9e04e081a90891f976
Deswandy <deswandywong23@gmail.com>
2025-08-09 08:28:04 +0700

    feat: error page diperbagus

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,13 @@ import { Button } from "@/components/ui/button";
 import Logo from "@/assets/images/logoDivisi/nexus.png";
 import { useNavigate } from "react-router";
 import { ArrowRight } from "lucide-react";
-import backgroundImage from "@/assets/images/onboarding.webp"
+import backgroundImage from "@/assets/images/onboarding.webp";
+import CircularText from "@/components/CircularText/CircularText";
 
 const ErrorPage = () => {
   const navigate = useNavigate();
   const handleClick = () => {
-    navigate("/login");
+    navigate("/");
   };
   return (
     <section
@@ -21,7 +22,15 @@ const ErrorPage = () => {
       }}
     >
       <div className="flex flex-col justify-center items-center">
-        <img className="w-34 z-1 md:w-40 drop-shadow-2xl" src={Logo} />
+        <div className="relative justify-center flex flex-col items-center">
+          <img className="absolute w-30 z-1 drop-shadow-2xl" src={Logo} />
+          <CircularText
+            text="NEXUS*MAXIMA*2025*"
+            onHover="speedUp"
+            spinDuration={40}
+            className="custom-class"
+          />
+        </div>
         <div className="flex flex-col mt-2 gap-2 drop-shadow-2xl">
           <h1 className="font-fraunces text-white text-center text-5xl font-semibold">
             404 NOT FOUND
@@ -30,9 +39,9 @@ const ErrorPage = () => {
             Hai, kami Nexus, dan kamu nyasar.
           </h1>
         </div>
-          <Button className="mt-2" variant="secondary" onClick={handleClick}>
-            Kembali ke website <ArrowRight />
-          </Button>
+        <Button className="mt-2" variant="clay" onClick={handleClick}>
+          Kembali ke website <ArrowRight />
+        </Button>
       </div>
     </section>
   );
