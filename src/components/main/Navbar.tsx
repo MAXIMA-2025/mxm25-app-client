3bd73040ba03349a888597a7e9bbf303b36cbc76
Deswandy <deswandywong23@gmail.com>
2025-08-12 16:32:23 +0700

    feat: logout

--- src/components/main/Navbar.tsx
+++ src/components/main/Navbar.tsx
@@ -9,9 +9,10 @@ import {
   MenubarTrigger,
 } from "@/components/ui/menubar";
 import { Button } from "../ui/button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import {
   HomeIcon,
+  LogOutIcon,
   Ticket,
   TicketCheck,
   TicketIcon,
@@ -19,10 +20,29 @@ import {
 } from "lucide-react";
 import logo from "/favicon.png";
 import { useToggle } from "@/contexts/ToggleContext";
+import {  useMutation, useQueryClient } from "@tanstack/react-query";
+import useApi from "@/hooks/useApi";
+import useAuthContext from "@/hooks/useAuthContext";
 
 const Navbar = () => {
   const { toggleAcara } = useToggle();
+  const queryClient = useQueryClient(); 
+  const auth = useAuthContext();
+  const api = useApi();
+  const nav = useNavigate(); 
   const target = toggleAcara?.find((t) => t.nama === "Station");
+    const mutation = useMutation({
+    mutationFn: async () => {
+      const res = await api.post("/auth/logout");
+      if (res.status !== 200) throw new Error("Gagal logout!");
+    },
+    onSuccess: () => {
+      queryClient.removeQueries({ queryKey: ["authUser"] });
+      auth.setIsLoggedOut(true);
+      nav("/");
+    },
+  });
+
   return (
     <nav className="fixed bottom-4 z-50">
       <Menubar className="shadow-2xl/100 ">
@@ -72,6 +92,21 @@ const Navbar = () => {
             </MenubarMenu>
           </>
         )}
+            <MenubarMenu>
+              <MenubarTrigger >
+                <div onClick={() => mutation.mutate()} className="flex gap-1 items-center justify-center hover:cursor-pointer"><LogOutIcon className="size-5"/>Log Out</div>
+              </MenubarTrigger>
+              {/* <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent> */}
+            </MenubarMenu>
       </Menubar>
     </nav>
   );
