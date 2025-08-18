7c952918c4bfa3baa9895b23959a9a7b728b8cf9
Deswandy <deswandywong23@gmail.com>
2025-08-18 21:44:46 +0700

    feat: about maxima

--- src/components/landing/AboutSection.tsx
+++ src/components/landing/AboutSection.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import backgroundImg from "@/assets/images/hero/ABOUT.webp";
 import { Button } from "@/components/ui/button";
-import about1 from "";
+import about1 from "@/assets/images/hero/about/about1.webp";
+import about2 from "@/assets/images/hero/about/about2.webp";
+import about3 from "@/assets/images/hero/about/about3.webp";
+import about4 from "@/assets/images/hero/about/about4.webp";
 
 const AboutSection = () => {
   return (
@@ -11,12 +14,42 @@ const AboutSection = () => {
         className="absolute h-full inset-0 w-full object-cover z-10 object-top"
         alt="Hero Background"
       />
-      <div className="z-20">
-        <div className="backdrop-blur-2xl w-full rounded-2xl p-4">
-          <h1 className="font-fraunces text-4xl font-semibold">Apa itu MAXIMA?</h1>
-          <div>
-            
+      <div className="z-20 flex flex-col items-center justify-center">
+        <h1
+          className="font-fraunces text-center text-4xl font-semibold  drop-shadow-xl mb-4
+             [text-shadow:1px_1px_0_white,_-1px_-1px_0_white,_1px_-1px_0_white,_-1px_1px_0_white]"
+        >
+          Apa itu MAXIMA?
+        </h1>
+        <div className="backdrop-blur-2xl w-[80%] gap-6 flex flex-col items-center justify-center rounded-2xl p-4 border-4 border-white/60 shadow-2xl">
+          <p className="font-futura text-center text-xl text-blue-950 font-medium">
+            Malam Ekspresi Mahasiswa atau MAXIMA merupakan rangkaian acara
+            terbesar di Universitas Multimedia Nusantara yang berada di bawah
+            naungan BEM. Kegiatan ini bertujuan membantu para mahasiswa baru
+            untuk dapat mengenal dan mempelajari organisasi yang terdapat di
+            dunia perkuliahan.
+          </p>
+          <div className="w-full flex flex-col md:flex-row gap-4 items-center justify-center">
+            <img
+              src={about1}
+              className="w-full md:w-[23%] rounded-lg md:rounded-2xl shadow-2xl drop-shadow-2xl border-3 md:border-4 border-white hover:-translate-y-1 transition-transform"
+            />
+            <img
+              src={about2}
+              className="w-full md:w-[23%] rounded-lg md:rounded-2xl shadow-2xl drop-shadow-2xl border-3 md:border-4 border-white hover:-translate-y-1 transition-transform"
+            />
+            <img
+              src={about3}
+              className="w-full md:w-[23%] rounded-lg md:rounded-2xl shadow-2xl drop-shadow-2xl border-3 md:border-4 border-white hover:-translate-y-1 transition-transform"
+            />
+            <img
+              src={about4}
+              className="w-full md:w-[23%] rounded-lg md:rounded-2xl shadow-2xl drop-shadow-2xl border-3 md:border-4 border-white hover:-translate-y-1 transition-transform"
+            />
           </div>
+          <Button variant="clay">
+            Bergabung bersama!
+          </Button>
         </div>
       </div>
     </section>
