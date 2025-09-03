0c940d9c526a62acea9d4604c38af51c0b2cf30c
rafpoo <163509148+rafpoo@users.noreply.github.com>
2025-09-03 16:56:38 +0700

    feat: tunggu link zoom bru

--- src/components/state/FilledState.tsx
+++ src/components/state/FilledState.tsx
@@ -81,8 +81,7 @@ const FilledState: React.FC<FilledStateProps> = ({
         "https://us06web.zoom.us/j/82796953234?pwd=V75D27o2prDVafKxYx8PZXaGRWLX8f.1";
       break;
     case 5:
-      linkZoom =
-        "https://us06web.zoom.us/j/87880182631?pwd=oN77Opc2w0ai2KghJLScRaru9ob9Ai.1";
+      linkZoom = "";
       break;
     case 6:
       linkZoom = `https://zoom.us`;
