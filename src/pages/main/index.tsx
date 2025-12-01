92e5902c7ec4944d74338e65989e76ed74cf9693
Rafael Nicholas Po <163509148+rafpoo@users.noreply.github.com>
2025-12-02 02:20:54 +0700

    feat: tes

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
-  return (
-    <div>main window</div>
-  )
-}
+  return <div>main windowssssssssssssssssss</div>;
+};
 
-export default index
+export default index;
